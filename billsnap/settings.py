0649afa46df5314db09eb3e88595d46bbcaf4fd8
Sai Pavan Saketh <64682070+saketh33@users.noreply.github.com>
2022-04-25 18:05:55 +0530

    first commit

--- billsnap/settings.py
+++ billsnap/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*&5y!7kegowd+pvd&blo2-&gmy51+p%1a7je-=-9temw44!8ye
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
